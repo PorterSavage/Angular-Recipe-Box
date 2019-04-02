9e6e4c1f263e83ad056b8f2444bfa03273cf684d
Jimmy Zebroski and Porter Savage <porter@>
2019-04-02 14:11:04 -0700

    Make recipe box application forangular practice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from './models/recipe.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Recipe Box';
+  recipe: string = 'Recipe List';
+  currentTime = new Date();
+  month: number = this.currentTime.getMonth() + 1;
+  day: number = this.currentTime.getDate();
+  year: number = this.currentTime.getFullYear();
+  recipes: Recipe[] = [];
+
+  addRecipe(newName: string, newIngredients: string, newDirections: string) {
+    const newIngredientsArray = newIngredients.split(", ");
+    const newDirectionsArray = newDirections.split(", ");
+    const newRecipes = new Recipe(newName, newIngredientsArray, newDirectionsArray);
+    this.recipes.push(newRecipes);
+  }
 }
+
+
+
